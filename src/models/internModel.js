5b30eb0f2a786bbf47e4035ae7b35ef784a2075d
AnugyaBansal <bansalanugya66@gmail.com>
2022-06-30 18:29:29 +0530

    Project2

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -28,6 +28,6 @@ const internSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     }
-}, { timestamps: true })
+})
 
 module.exports = mongoose.model('intern', internSchema)
